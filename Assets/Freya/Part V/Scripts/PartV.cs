589240c0db8840087934f7eb5f2901115954f9be
AsyncOperator <nhakan6parmak@gmail.com>
2025-12-26 02:53:43 +0300

    Mesh generation

--- Assets/Freya/Part V/Scripts/PartV.cs
+++ Assets/Freya/Part V/Scripts/PartV.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Freya.Part_V.Scripts
@@ -9,6 +10,9 @@ namespace Freya.Part_V.Scripts
     {
         [SerializeField] private Rigidbody m_Rigidbody;
         [SerializeField] private float m_Speed;
+        [SerializeField] private MeshFilter m_MeshFilter;
+
+        private Mesh m_Mesh;
 
         private Vector3 Position => transform.position;
         private Vector3 Velocity => transform.right * m_Speed;
@@ -22,6 +26,41 @@ namespace Freya.Part_V.Scripts
         private Vector3 GetPointAtTrajectory(float t)
         {
             return Position + Velocity * t + (Acceleration * 0.5f) * t * t;
+        }
+
+        [ContextMenu("Generate Mesh")]
+        private void GenerateMesh()
+        {
+            if (m_Mesh == null)
+            {
+                m_Mesh = new Mesh();
+                m_MeshFilter.sharedMesh = m_Mesh;
+            }
+
+            List<Vector3> verts = new List<Vector3>()
+            {
+                new Vector3(0.0f, 0.0f, 0.0f),
+                new Vector3(0.0f, 0.0f, 1.0f),
+                new Vector3(1.0f, 0.0f, 1.0f),
+                new Vector3(1.0f, 0.0f, 0.0f)
+            };
+            List<int> tris = new List<int>()
+            {
+                3, 0, 1, 3, 1, 2
+            };
+            List<Vector3> vertsNormals = new List<Vector3>()
+            {
+                Vector3.up,
+                Vector3.up,
+                Vector3.up,
+                Vector3.up
+            };
+
+            m_Mesh.Clear();
+            m_Mesh.SetVertices(verts);
+            m_Mesh.SetTriangles(tris, 0);
+            m_Mesh.SetNormals(vertsNormals);
+            // m_Mesh.RecalculateNormals();
         }
 
         private void OnDrawGizmos()
