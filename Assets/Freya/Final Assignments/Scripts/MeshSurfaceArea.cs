74fb8507f545aeb036d93e6077b4d630c7b8ac23
AsyncOperator <nhakan6parmak@gmail.com>
2025-12-28 23:47:17 +0300

    Mesh volume calculation

--- Assets/Freya/Final Assignments/Scripts/MeshSurfaceArea.cs
+++ Assets/Freya/Final Assignments/Scripts/MeshSurfaceArea.cs
@@ -6,6 +6,12 @@ namespace Freya.Final_Assignments.Scripts
     {
         [SerializeField] private Mesh m_Mesh;
 
+        // https://stackoverflow.com/questions/1406029/how-to-calculate-the-volume-of-a-3d-mesh-object-the-surface-of-which-is-made-up
+        private float SignedVolumeOfTriangle(Vector3 p0, Vector3 p1, Vector3 p2)
+        {
+            return Vector3.Dot(p0, Vector3.Cross(p1, p2)) / 6.0f;
+        }
+
         private float CalculateTriangleAreaByHeronFormula(Vector3 p0, Vector3 p1, Vector3 p2)
         {
             float side0 = (p0 - p1).magnitude;
@@ -44,8 +50,9 @@ namespace Freya.Final_Assignments.Scripts
             int[] tris = m_Mesh.triangles;
             Vector3[] vertices = m_Mesh.vertices;
 
-            float accumulatedArea0 = 0.0f;
-            float accumulatedArea1 = 0.0f;
+            float accumulatedAreaHeron = 0.0f;
+            float accumulatedAreaDot = 0.0f;
+            float accumulatedVolume = 0.0f;
             for (int i = 0; i < tris.Length; i += 3)
             {
                 int t0 = tris[i + 0];
@@ -56,12 +63,14 @@ namespace Freya.Final_Assignments.Scripts
                 Vector3 p1 = vertices[t1];
                 Vector3 p2 = vertices[t2];
 
-                accumulatedArea0 += CalculateTriangleAreaByHeronFormula(p0, p1, p2);
-                accumulatedArea1 += CalculateTriangleAreaByDotProduct(p0, p1, p2);
+                accumulatedAreaHeron += CalculateTriangleAreaByHeronFormula(p0, p1, p2);
+                accumulatedAreaDot += CalculateTriangleAreaByDotProduct(p0, p1, p2);
+                accumulatedVolume += SignedVolumeOfTriangle(p0, p1, p2);
             }
 
-            Debug.Log($"[Heron's formula] {m_Mesh.name} accumulated area: {accumulatedArea0}");
-            Debug.Log($"[Dot] {m_Mesh.name} accumulated area: {accumulatedArea1}");
+            Debug.Log($"[Heron's formula] {m_Mesh.name} accumulated area: {accumulatedAreaHeron}");
+            Debug.Log($"[Dot] {m_Mesh.name} accumulated area: {accumulatedAreaDot}");
+            Debug.Log($"<color=green>{m_Mesh.name} accumulated volume:</color> {accumulatedVolume}");
         }
     }
 }
