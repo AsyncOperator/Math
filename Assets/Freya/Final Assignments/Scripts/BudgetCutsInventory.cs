2342ea785bfc308368c68809f44de37a25c0eebe
AsyncOperator <nhakan6parmak@gmail.com>
2026-01-11 18:44:51 +0300

    Fix: Budget Cuts Inventory assignment

--- Assets/Freya/Final Assignments/Scripts/BudgetCutsInventory.cs
+++ Assets/Freya/Final Assignments/Scripts/BudgetCutsInventory.cs
@@ -6,36 +6,64 @@ namespace Freya.Final_Assignments.Scripts
     public class BudgetCutsInventory : MonoBehaviour
     {
         [SerializeField] private float[] m_ItemsRadii;
-        [SerializeField] private float m_Distance;
+        [SerializeField] private float m_ArcRadii;
 
-        private float GetAngleForRadii(float radii)
+        private void OnValidate()
         {
-            float a = Mathf.Asin(radii * 0.5f / m_Distance);
+            int length = m_ItemsRadii.Length;
+            for (int i = 0; i < length; i++)
+            {
+                m_ItemsRadii[i] = Mathf.Max(m_ItemsRadii[i], 0.0f);
+            }
 
-            return 4 * a * Mathf.Rad2Deg;
+            m_ArcRadii = Mathf.Max(m_ArcRadii, 1.0f);
+        }
+
+        private float LawOfCosines(float sideA, float sideB, float sideC)
+        {
+            float cosC = (sideA * sideA + sideB * sideB - sideC * sideC) / (2 * sideA * sideB);
+            return Mathf.Acos(Mathf.Clamp(cosC, -1.0f, 1.0f)) * Mathf.Rad2Deg;
+        }
+
+        private float GetAngleBetweenCircles(float radii0, float radii1)
+        {
+            float sum = radii0 + radii1;
+            return LawOfCosines(m_ArcRadii, m_ArcRadii, sum);
         }
 
         private void OnDrawGizmos()
         {
+            Handles.matrix = transform.localToWorldMatrix;
+
             float angle = 0.0f;
-            for (int i = 0; i < m_ItemsRadii.Length; i++)
+            for (int i = 0; i < m_ItemsRadii.Length - 1; i++)
             {
-                angle += GetAngleForRadii(m_ItemsRadii[i]);
+                float radii0 = m_ItemsRadii[i];
+                float radii1 = m_ItemsRadii[i + 1];
+                angle += GetAngleBetweenCircles(radii0, radii1);
             }
 
-            Handles.matrix = transform.localToWorldMatrix;
-            Handles.DrawWireArc(Vector3.zero, Vector3.back, Quaternion.AngleAxis(angle * 0.5f, Vector3.forward) * Vector3.up, angle, m_Distance);
+            float offset = GetAngleBetweenCircles(m_ItemsRadii[0], Mathf.Epsilon);
+            angle += offset + GetAngleBetweenCircles(m_ItemsRadii[^1], Mathf.Epsilon);
 
-            angle *= 0.5f;
+            Vector3 startDirection = Quaternion.AngleAxis(angle * 0.5f, Vector3.forward) * Vector3.up;
+            Vector3 endDirection = Quaternion.AngleAxis(-angle * 0.5f, Vector3.forward) * Vector3.up;
+            Handles.DrawLine(Vector3.zero, startDirection * m_ArcRadii);
+            Handles.DrawLine(Vector3.zero, endDirection * m_ArcRadii);
+            Handles.DrawWireArc(Vector3.zero, Vector3.back, startDirection, angle, m_ArcRadii);
+
+            angle = angle * 0.5f - offset;
 
             for (int i = 0; i < m_ItemsRadii.Length; i++)
             {
-                float radii = m_ItemsRadii[i];
-                float halfAngle = GetAngleForRadii(radii) * 0.5f;
-                angle -= halfAngle;
-                Vector3 center = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.up * m_Distance;
-                Handles.DrawWireDisc(center, Vector3.back, radii);
-                angle -= halfAngle;
+                float radii0 = m_ItemsRadii[i];
+                float radii1 = i < m_ItemsRadii.Length - 1 ? m_ItemsRadii[i + 1] : 0.0f;
+
+                Vector3 direction = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.up;
+                Vector3 center = direction * m_ArcRadii;
+                Handles.DrawWireDisc(center, Vector3.back, radii0);
+
+                angle -= GetAngleBetweenCircles(radii0, radii1);
             }
         }
     }
